76e3f491f928ad98a68de5510274c56c0b682ae4
man90 <man90@tuta.io>
2022-07-07 13:55:12 +0200

    Update SA translations

--- translators/TranslateClassName.go
+++ translators/TranslateClassName.go
@@ -1,20 +1,24 @@
 package translators
 
 var classNameTranslationMap = map[string]string{
-	"Cavaleira Negra": "Dark Knight",
-	"Corsária":        "Corsair",
-	"Domadora":        "Tamer",
-	"Feiticeira":      "Sorceress",
-	"Guardiã":         "Guardian",
-	"Guerreiro":       "Warrior",
-	"Lutador":         "Striker",
-	"Maga":            "Witch",
-	"Mago":            "Wizard",
-	"Mística":         "Mystic",
-	"Musah":           "Musa",
-	"Sábio":           "Sage",
-	"Sagitário":       "Archer",
-	"Valquíria":       "Valkyrie",
+	"Arqueiro":             "Archer",
+	"Bruxa":                "Witch",
+	"Caçadora":             "Ranger",
+	"Cavaleira das Trevas": "Dark Knight",
+	"Cavaleira Negra":      "Dark Knight",
+	"Corsária":             "Corsair",
+	"Domadora":             "Tamer",
+	"Feiticeira":           "Sorceress",
+	"Guardiã":              "Guardian",
+	"Guerreiro":            "Warrior",
+	"Lutador":              "Striker",
+	"Maga":                 "Witch",
+	"Mago":                 "Wizard",
+	"Mística":              "Mystic",
+	"Musah":                "Musa",
+	"Sábio":                "Sage",
+	"Sagitário":            "Archer",
+	"Valquíria":            "Valkyrie",
 }
 
 func TranslateClassName(className *string) {
