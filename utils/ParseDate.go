072b82cbd9520ec8de9f89c372bba779c9f51b63
man90 <man90@tuta.io>
2024-05-15 20:13:26 +0200

    Update date parser

--- utils/ParseDate.go
+++ utils/ParseDate.go
@@ -8,17 +8,14 @@ import (
 func ParseDate(text string) time.Time {
 	var format string
 	if strings.Contains(text, ".") {
-		// Used on KR server for account creation date
+		// KR
 		format = "2006.01.02"
 	} else if strings.Contains(text, "/") {
-		// Used on SA server for account creation date
-		format = "02/01/2006"
-	} else if strings.Contains(text, "-") {
-		// Used on all servers for guild creation date
-		format = "2006-01-02"
+		// SA
+		format = "02/01/2006 (UTC-3)"
 	} else {
-		// Used on NAEU server for account creation date
-		format = "Jan 2, 2006"
+		// NAEU
+		format = "Jan 2, 2006 (UTC)"
 	}
 
 	if parsed, err := time.Parse(format, RemoveExtraSpaces(text)); nil == err {
