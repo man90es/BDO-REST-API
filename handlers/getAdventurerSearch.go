63ddd477ea4caf0d2aa7911257f9f3f2d4c86ff6
man90 <man90@tuta.io>
2024-08-29 19:44:37 +0200

    Fix crash when searchType is not provided

--- handlers/getAdventurerSearch.go
+++ handlers/getAdventurerSearch.go
@@ -19,7 +19,7 @@ func getAdventurerSearch(w http.ResponseWriter, r *http.Request) {
 	query, queryOk := validators.ValidateAdventurerNameQueryParam(r.URL.Query()["query"])
 	region, regionOk := validators.ValidateRegionQueryParam(r.URL.Query()["region"])
 	searchTypeQueryParam := r.URL.Query()["searchType"]
-	searchType := validators.ValidateSearchTypeQueryParam(searchTypeQueryParam)
+	searchType, searchTypeAsString := validators.ValidateSearchTypeQueryParam(searchTypeQueryParam)
 
 	if !queryOk || !regionOk {
 		giveBadRequestResponse(w)
@@ -34,7 +34,7 @@ func getAdventurerSearch(w http.ResponseWriter, r *http.Request) {
 	query = strings.ToLower(query)
 
 	// Look for cached data, then run the scraper if needed
-	data, status, date, expires, found := profileSearchCache.GetRecord([]string{region, query, searchTypeQueryParam[0], fmt.Sprint(page)})
+	data, status, date, expires, found := profileSearchCache.GetRecord([]string{region, query, searchTypeAsString, fmt.Sprint(page)})
 	if !found {
 		data, status = scrapers.ScrapeAdventurerSearch(region, query, searchType, page)
 
@@ -47,7 +47,7 @@ func getAdventurerSearch(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		date, expires = profileSearchCache.AddRecord([]string{region, query, searchTypeQueryParam[0], fmt.Sprint(page)}, data, status)
+		date, expires = profileSearchCache.AddRecord([]string{region, query, searchTypeAsString, fmt.Sprint(page)}, data, status)
 	}
 
 	w.Header().Set("Date", date)
