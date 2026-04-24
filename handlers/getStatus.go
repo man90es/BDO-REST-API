3570632e1298089f655d83bf4727953f466beeba
man90 <man90@tuta.io>
2026-04-24 20:10:05 +0200

    Bump up version number

--- handlers/getStatus.go
+++ handlers/getStatus.go
@@ -12,7 +12,7 @@ import (
 )
 
 var initTime = time.Now()
-var version = "1.18.2"
+var version = "1.18.3"
 
 func getStatus(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]interface{}{
