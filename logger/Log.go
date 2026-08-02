a52cb84b474516ec9766f4dd197bb57213daf59e
man90 <man90@tuta.io>
2026-08-02 11:55:15 +0200

    Implement adding instance ID to logs

--- logger/Log.go
+++ logger/Log.go
@@ -3,7 +3,8 @@ package logger
 import "time"
 
 type Log struct {
-	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
-	Level     string    `json:"level" bson:"level"`
-	Message   string    `json:"message" bson:"message"`
+	CreatedAt  time.Time `json:"createdAt" bson:"createdAt"`
+	InstanceID string    `json:"instanceId" bson:"instanceId"`
+	Level      string    `json:"level" bson:"level"`
+	Message    string    `json:"message" bson:"message"`
 }
