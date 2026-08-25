7fc233f19a75c8e4a0ae3ea06d734570c674803b
Max Blazejewski <man90@tuta.io>
2026-08-25 20:11:45 +0200

    Fix unbounded profileTarget parameter length (#85)

--- handlers/getStatus.go
+++ handlers/getStatus.go
@@ -12,7 +12,7 @@ import (
 )
 
 var initTime = time.Now()
-var version = "1.20.1"
+var version = "1.20.2"
 
 func getStatus(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]interface{}{
