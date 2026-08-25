2f9bbf17585185b9069a4f4083a74a1fc90ccf1a
man90 <man90@tuta.io>
2026-08-25 20:10:23 +0200

    Fix unbounded profileTarget parameter length

--- validators/ValidateProfileTargetQueryParam.go
+++ validators/ValidateProfileTargetQueryParam.go
@@ -1,15 +1,14 @@
 package validators
 
-// Check that the length is at least 150 characters
-// I don't actually know how long it should be, but the length varies
+// Check that the length is between 150 and 250 characters
 func ValidateProfileTargetQueryParam(query []string) (profileTarget string, ok bool, errorMessage string) {
 	if 1 > len(query) {
 		return "", false, "Profile target is missing from the request"
 	}
 
-	if ok := len(query[0]) >= 150; ok {
+	if ok := len(query[0]) > 150 && len(query[0]) < 250; ok {
 		return query[0], true, ""
 	}
 
-	return query[0], false, "Profile target has to be at least 150 characters long"
+	return query[0], false, "Profile target has to be between 150 and 250 characters long"
 }
--- validators/ValidateProfileTargetQueryParam_test.go
+++ validators/ValidateProfileTargetQueryParam_test.go
@@ -9,20 +9,24 @@ func TestValidateProfileTargetQueryParam(t *testing.T) {
 		expectedMessage string
 		input           []string
 	}{
-		// Valid profile targets with lengths >= 150
-		{input: []string{repeat("A", 150)}, expectedPT: repeat("A", 150), expectedOk: true, expectedMessage: ""},
+		// Valid profile targets with lengths > 150
+		{input: []string{repeat("A", 151)}, expectedPT: repeat("A", 151), expectedOk: true, expectedMessage: ""},
 		{input: []string{repeat("A", 200)}, expectedPT: repeat("A", 200), expectedOk: true, expectedMessage: ""},
 
 		// Invalid profile targets with lengths < 150
-		{input: []string{""}, expectedPT: "", expectedOk: false, expectedMessage: "Profile target has to be at least 150 characters long"},
-		{input: []string{"Short"}, expectedPT: "Short", expectedOk: false, expectedMessage: "Profile target has to be at least 150 characters long"},
-		{input: []string{repeat("A", 149)}, expectedPT: repeat("A", 149), expectedOk: false, expectedMessage: "Profile target has to be at least 150 characters long"},
+		{input: []string{""}, expectedPT: "", expectedOk: false, expectedMessage: "Profile target has to be between 150 and 250 characters long"},
+		{input: []string{"Short"}, expectedPT: "Short", expectedOk: false, expectedMessage: "Profile target has to be between 150 and 250 characters long"},
+		{input: []string{repeat("A", 149)}, expectedPT: repeat("A", 149), expectedOk: false, expectedMessage: "Profile target has to be between 150 and 250 characters long"},
+
+		// Invalid profile targets with length >= 250
+		{input: []string{repeat("A", 250)}, expectedPT: repeat("A", 250), expectedOk: false, expectedMessage: "Profile target has to be between 150 and 250 characters long"},
+		{input: []string{repeat("A", 300)}, expectedPT: repeat("A", 300), expectedOk: false, expectedMessage: "Profile target has to be between 150 and 250 characters long"},
 
 		// Query param not provided
 		{input: []string{}, expectedPT: "", expectedOk: false, expectedMessage: "Profile target is missing from the request"},
 
 		// Several profileTargets provided
-		{input: []string{repeat("A", 150), repeat("B", 150)}, expectedPT: repeat("A", 150), expectedOk: true, expectedMessage: ""},
+		{input: []string{repeat("A", 151), repeat("B", 151)}, expectedPT: repeat("A", 151), expectedOk: true, expectedMessage: ""},
 	}
 
 	for _, test := range tests {
